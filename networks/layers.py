db83c8740f0d3aa5d3abc37fa543aa41b7eedc73
Ea0011 <avagyanee@gmail.com>
2022-03-17 15:31:52 +0400

    fix: turn off corner alignment that depended on size

--- networks/layers.py
+++ networks/layers.py
@@ -73,7 +73,7 @@ class UNetDecoderBlock(nn.Module):
   def __init__(self, in_c, out_c):
     super().__init__()
     self.up = nn.ConvTranspose2d(in_c, out_c, kernel_size=2, stride=2, padding=0)
-    self.interpolate = nn.Upsample(scale_factor=2, mode="bilinear")
+    self.interpolate = nn.Upsample(scale_factor=2, mode="bilinear", align_corners=False)
     self.conv = ConvBlock(2 * out_c, out_c)
     self.conv_1 = nn.Conv2d(in_c, out_c, kernel_size=1)
     self.norm = nn.InstanceNorm2d(out_c, affine=True)
