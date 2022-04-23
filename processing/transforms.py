7b00fcb18f440cb167e76bfc1c0c60daeb0f7da3
Ea0011 <avagyanee@gmail.com>
2022-04-23 19:30:44 +0400

    fix: more binarized sketches

--- processing/transforms.py
+++ processing/transforms.py
@@ -24,7 +24,7 @@ class RandomSketch():
     self.params[-2] = 0.9
     self.params[1] = np.random.choice([400, 800], 1)[0]
     self.params[-3] = np.random.uniform(1.4, 1.6)
-    self.params[-1] = np.random.choice([True, False], 1)[0]
+    self.params[-1] = np.random.choice([True, False], 1, p=[0.7, 0.3])[0]
     sketch = xdog(image, *self.params)
 
     return sketch
